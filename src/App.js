e8350b8a5b83874324145bbdf6e554fd6f80530b
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-12-19 19:17:55 -0300

    Learn API in react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Consumir a API
+    fetch('http://192.168.1.67:8080/customer')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Erro ao buscar dados');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCustomers(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []); // Array vazio para rodar apenas uma vez após o carregamento inicial
+
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (error) {
+    return <div>Erro: {error}</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>VAMOS CONSUMIR API</p>
+        <ul>
+          {customers.map((customer) => (
+            <li key={customer.id}>
+              {customer.name} (ID: {customer.id})
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
